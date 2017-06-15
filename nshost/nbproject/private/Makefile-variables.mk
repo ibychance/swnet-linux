5f668b5267a8b7e73a6162f1f5bad3ea58db3a74
zhuoyz <250603123@qq.com>
2017-06-15 21:18:16 +0800

    清理整个工程目录

--- nshost/nbproject/private/Makefile-variables.mk
+++
@@ -1,9 +0,0 @@
-#
-# Generated - do not edit!
-#
-# NOCDDL
-#
-# Debug configuration
-# Debug_x86_64 configuration
-# Release configuration
-# Release_x86_64 configuration
--- nsp/nbproject/private/Makefile-variables.mk
+++
@@ -1,9 +0,0 @@
-#
-# Generated - do not edit!
-#
-# NOCDDL
-#
-# Debug configuration
-# Release configuration
-# Debug_x86_64 configuration
-# Release_x86_64 configuration
