1d351c6816a96cdd4b1c5403142ccc7208ffe826
zhuoyunzhi <250603123@qq.com>
2018-12-12 15:28:11 +0800

    修改tcpal部分备注为英文

--- tcpal.c
+++ tcpal.c
@@ -15,30 +15,29 @@ int tcp_parse_pkt(ncb_t *ncb, const char *data, int cpcb) {
 
     cpbuff = data;
 
-    /* 没有指定包头模板， 直接回调整个TCP包 */
+    /* no template specified, direct give the whole packet */
     if (0 == ncb->u.tcp.template.cb_ || !ncb->u.tcp.template.parser_) {
         ncb_post_recvdata(ncb, cpcb, data);
         ncb->u.tcp.rx_parse_offset = 0;
         return 0;
     }
 
-    /*已经被标记为大包接收状态 ，则处理大包数据*/
+    /* it is in the large-block status */
     if (ncb_lb_marked(ncb)) {
-        /*本次到达数据尚不足以填满大包*/
+        /* The arrival data are not enough to fill the large-block. */
         if (cpcb + ncb->u.tcp.lboffset < ncb->u.tcp.lbsize) {
             memcpy(ncb->u.tcp.lbdata + ncb->u.tcp.lboffset, cpbuff, cpcb);
             ncb->u.tcp.lboffset += cpcb;
             return 0;
         }
 
-        /*本次到达数据足以完成大包*/
+        /* The arrival data are it's enough to fill the large-block. */
         overplus = ncb->u.tcp.lbsize - ncb->u.tcp.lboffset;
         memcpy(ncb->u.tcp.lbdata + ncb->u.tcp.lboffset, cpbuff, overplus);
 
-        /*完成组包, 回调给上层模块*/
         ncb_post_recvdata(ncb, ncb->u.tcp.lbsize - ncb->u.tcp.template.cb_, ncb->u.tcp.lbdata + ncb->u.tcp.template.cb_);
 
-        /*释放大包缓冲区*/
+        /* fre the large-block buffer */
         free(ncb->u.tcp.lbdata);
         ncb->u.tcp.lbdata = NULL;
         ncb->u.tcp.lboffset = 0;
@@ -46,7 +45,8 @@ int tcp_parse_pkt(ncb_t *ncb, const char *data, int cpcb) {
         return (cpcb - overplus);
     }
 
-    /*数据长度尚不足构成协议头, 全部数据用于构建协议头， 返回剩余长度0*/
+    /* the length of data is not enough to constitute the protocol header. 
+    *  All data is used to construct the protocol header and return the remaining length of 0. */
     if (ncb->u.tcp.rx_parse_offset + cpcb < ncb->u.tcp.template.cb_) {
         memcpy(ncb->packet + ncb->u.tcp.rx_parse_offset, cpbuff, cpcb);
         ncb->u.tcp.rx_parse_offset += cpcb;
@@ -65,35 +65,36 @@ int tcp_parse_pkt(ncb_t *ncb, const char *data, int cpcb) {
         ncb->u.tcp.rx_parse_offset = ncb->u.tcp.template.cb_;
     }
 
-    /* 底层协议交互给协议模板处理， 处理失败则解包操作无法继续 */
+    /* The low-level protocol interacts with the protocol template, and the unpacking operation cannot continue if the processing fails.  */
     if (!(*ncb->u.tcp.template.parser_)) {
         nis_call_ecr("nshost.tcpal.parse : parser tempalte method illegal.");
         return -1;
     }
 
-	/* 通过解释例程得到用户段数据长度 */
+	/* Get the length of user segment data by interpreting routines  */
     if ((*ncb->u.tcp.template.parser_)(ncb->packet, ncb->u.tcp.rx_parse_offset, &user_data_size) < 0) {
         nis_call_ecr("nshost.tcpal.parse : failed to parse template header.");
 		return -1;
 	}
 	
-	/* 如果用户数据长度超出最大容忍长度，则直接报告为错误, 有可能是恶意攻击 */
+	/* If the user data length exceeds the maximum tolerance length, 
+     * it will be reported as an error directly, possibly a malicious attack.  */
     if ((user_data_size > TCP_MAXIMUM_PACKET_SIZE) || (user_data_size <= 0)) {
         nis_call_ecr("nshost.tcpal.parse : bad data size:%d.", user_data_size);
 		return -1;
 	}
 	
-    /* 含包头的包总长度 */
+    /* total package length, include the packet head */
     total_packet_length = user_data_size + ncb->u.tcp.template.cb_;
 
-    /* 如果是大包，则应该建立大包流程 */
+    /* If it is a large-block, then we should establish a large-block process.  */
     if (total_packet_length > TCP_BUFFER_SIZE) {
         ncb->u.tcp.lbdata = (char *) malloc(total_packet_length);
         if (!ncb->u.tcp.lbdata) {
             return -1;
         }
 
-        /* 含底层协议包头的大包数据总长度 */
+        /* total large-block length, include the low-level protocol head length */
         ncb->u.tcp.lbsize = total_packet_length;
 
         /* 本次全部数据拷贝到大包缓冲区 */
