6b5d6e2a3c1458b0a39db8f7a2cea93148d0e08e
zhuoyunzhi <250603123@qq.com>
2018-12-12 15:30:25 +0800

    补充fifo文件

---
+++ fifo.c
@@ -0,0 +1,149 @@
+#include "fifo.h"
+
+#include <stdlib.h>
+#include <errno.h>
+
+#include "mxx.h"
+
+void fifo_init(ncb_t *ncb) {
+    struct tx_fifo *fifo;
+
+    if (ncb) {
+        fifo = &ncb->fifo;
+        fifo->blocking = 0;
+        fifo->size = 0;
+        posix__pthread_mutex_init(&fifo->lock);
+        INIT_LIST_HEAD(&fifo->head);
+    }
+}
+
+void fifo_uninit(ncb_t *ncb) {
+    struct tx_node *node;
+    struct tx_fifo *fifo;
+
+    if (ncb) {
+        fifo = &ncb->fifo;
+        posix__pthread_mutex_lock(&fifo->lock);
+        while ((node = list_first_entry_or_null(&fifo->head, struct tx_node, link)) != NULL) {
+            list_del(&node->link);
+            if (node->data) {
+                free(node->data);
+            }
+            free(node);
+        }
+        posix__pthread_mutex_unlock(&fifo->lock);
+        posix__pthread_mutex_release(&fifo->lock);
+    }
+}
+
+int fifo_queue(ncb_t *ncb, struct tx_node *node) {
+    int n;
+    struct tx_fifo *fifo;
+
+    if (!ncb || !node) {
+        return -1;
+    }
+
+    fifo = &ncb->fifo;
+    n = -1;
+
+    posix__pthread_mutex_lock(&fifo->lock);
+    do {
+        if (fifo->size >= MAXIMUM_NCB_FIFO_SIZE) {
+            break;
+        }
+        list_add_tail(&node->link, &fifo->head);
+        n = ++fifo->size;
+
+        /* queue item into fifo.
+         * it is ensure the EAGAIN event was happened.
+         * the calling thread should change the epoll to EPOLLOUT mode */
+        if (0 == fifo->blocking) {
+            fifo->blocking = 1;
+            iomod(ncb, EPOLLIN | EPOLLOUT);
+            nis_call_ecr("nshost.fifo:link %lld set IO blocking.", ncb->hld);
+        }
+    } while(0);
+    
+    posix__pthread_mutex_unlock(&fifo->lock);
+    return n;
+}
+
+int fifo_top(ncb_t *ncb, struct tx_node **node) {
+    struct tx_node *front;
+    struct tx_fifo *fifo;
+
+    if (!ncb || !node) {
+        return -1;
+    }
+
+    fifo = &ncb->fifo;
+    front = NULL;
+
+    posix__pthread_mutex_lock(&fifo->lock);
+    if (NULL != (front = list_first_entry_or_null(&fifo->head, struct tx_node, link))) {
+        *node = front;
+    }
+    posix__pthread_mutex_unlock(&fifo->lock);
+
+    return ((NULL == front) ? -1 : 0);
+}
+
+int fifo_pop(ncb_t *ncb, struct tx_node **node) {
+    struct tx_node *front;
+    struct tx_fifo *fifo;
+    int remain;
+
+    if (!ncb) {
+        return -1;
+    }
+
+    fifo = &ncb->fifo;
+    front = NULL;
+
+    posix__pthread_mutex_lock(&fifo->lock);
+    if (NULL != (front = list_first_entry_or_null(&fifo->head, struct tx_node, link))) {
+        list_del(&front->link);
+        INIT_LIST_HEAD(&front->link);
+        remain = --fifo->size;
+
+        /* the calling thread should change the epoll to EPOLLIN mode */
+        if ((0 == remain) && (1 == fifo->blocking)) {
+            fifo->blocking = 0;
+            iomod(ncb, EPOLLIN);
+            nis_call_ecr("nshost.fifo:link %lld cancel IO blocking.", ncb->hld);
+        }
+    }
+    posix__pthread_mutex_unlock(&fifo->lock);
+
+    if (front) {
+        if (node) {
+            *node = front;
+        }
+
+        if (front->data) {
+            free(front->data);
+        }
+        free(front);
+        return 1;
+    }
+
+    return 0;
+}
+
+int fifo_is_blocking(ncb_t *ncb) {
+    struct tx_fifo *fifo;
+    int blocking;
+
+    if (!ncb) {
+        return -1;
+    }
+
+    fifo = &ncb->fifo;
+
+    posix__pthread_mutex_lock(&fifo->lock);
+    blocking = fifo->blocking;
+    posix__pthread_mutex_unlock(&fifo->lock);
+
+    return blocking;
+}
---
+++ fifo.h
@@ -0,0 +1,40 @@
+#ifndef FQUE_H_20170118
+#define FQUE_H_20170118
+
+#include <netinet/in.h>
+
+#include "ncb.h"
+
+struct tx_node {
+    unsigned char *data; /* data buffer for Tx */
+    int wcb; /* the total count of bytes need to write */
+    int offset; /* the current offset of success written */
+    struct sockaddr_in udp_target; /* the Tx target address, UDP only */
+    struct list_head link; 
+};
+
+extern void fifo_init(ncb_t *ncb);
+extern void fifo_uninit(ncb_t *ncb);
+
+/* queue Tx node into pnding fifo. 
+ * the io blocking status will be automatic recover to 1 */
+extern int fifo_queue(ncb_t *ncb, struct tx_node *node);
+
+/* get front item of current fifo */
+extern int fifo_top(ncb_t *ncb, struct tx_node **node);
+
+/* pop the front item from current fifo, option, you can get the top one 
+ * when the last item have been pop from the fifo, the io blocking status will be automatic recover to 0, 
+ * if @node is null, the front item memory will be free after pop 
+ * 
+ * the return value definition:
+ * -1 : error occurred 
+ * 0  : the queue is empty
+ * 1  : success pop top item 
+ */
+extern int fifo_pop(ncb_t *ncb, struct tx_node **node);
+
+/* test the current io block status, boolean return */
+extern int fifo_is_blocking(ncb_t *ncb);
+
+#endif
