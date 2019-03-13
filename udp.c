a287cbebfaa8b1a30b41983652f4410b0e7d3091
zhuoyunzhi <250603123@qq.com>
2019-03-13 15:00:57 +0800

    当TCP发送发生EAGAIN，同时发生本地缓冲区满的情况， 可能导致内存泄漏

--- udp.c
+++ udp.c
@@ -21,7 +21,7 @@ int udprefr( objhld_t hld, ncb_t **ncb ) {
         *ncb = NULL;
         return -EPROTOTYPE;
     }
-    
+
     return -ENOENT;
 }
 
@@ -133,13 +133,13 @@ HUDPLINK udp_create(udp_io_callback_t user_callback, const char* l_ipstr, uint16
         /* set data handler function pointer for Rx/Tx */
         ncb->ncb_read = &udp_rx;
         ncb->ncb_write = &udp_tx;
-        
+
         /* attach to epoll */
         retval = ioatth(ncb, EPOLLIN);
         if (retval < 0) {
             break;
         }
-        
+
         objdefr(hld);
         return hld;
     } while (0);
@@ -197,7 +197,7 @@ int udp_write(HUDPLINK lnk, const void *origin, int cb, const char* r_ipstr, uin
         } else {
             memcpy(buffer, origin, cb);
         }
-        
+
 
         node = (struct tx_node *) malloc(sizeof (struct tx_node));
         if (!node) {
@@ -215,27 +215,34 @@ int udp_write(HUDPLINK lnk, const void *origin, int cb, const char* r_ipstr, uin
         if (!fifo_is_blocking(ncb)) {
             retval = udp_txn(ncb, node);
 
-            /* 
+            /*
              * the return value means direct failed when it equal to -1 or success when it greater than zero.
              * in these case, destroy memory resource outside loop, no matter what the actually result it is.
              */
             if (-EAGAIN != retval) {
-                break; 
+                break;
             }
         }
 
-        /* 
+        /*
          * when the IO blocking is existed, we can't send data immediately,
          * only way to handler this situation is queued data into @wpool.
-         * otherwise, the wrong operation may broken the output sequence 
+         * otherwise, the wrong operation may broken the output sequence
          *
          * in case of -EAGAIN return by @udp_txn, means the write operation cannot be complete right now,
          * insert @node into the tail of @fifo queue, be careful, in this case, memory of @buffer and @node cannot be destroy until asynchronous completed
          *
          * just insert @node into tail of @fifo queue,  awaken write thread is not necessary.
-         * don't worry about the task thread notify, when success calling to @ncb_set_blocking, ensure that the @EPOLLOUT event can being captured by IO thread 
+         * don't worry about the task thread notify, when success calling to @ncb_set_blocking, ensure that the @EPOLLOUT event can being captured by IO thread
+         *
+         * the return value by calling @fifo_queue maybe -EBUSY
+         * in this case means the cache queue is full, no more items canbe insert into @fifo of this ncb, package will be drop
          */
-        fifo_queue(ncb, node);
+        retval = fifo_queue(ncb, node);
+        if (retval < 0) {
+            break;
+        }
+
         objdefr(lnk);
         return 0;
     } while (0);
@@ -322,8 +329,8 @@ int udp_get_boardcast(ncb_t *ncb, int *enabled) {
  * 然而在ip组播环中，数据包的目的地址不是一个，而是一组，形成组地址。所有的信息接收者都加入到一个组内，并且一旦加入之后，流向组地址的数据立即开始向接收者传输，组中的所有成员都能接收到数据包。
  * 组播组中的成员是动态的，主机可以在任何时刻加入和离开组播组。
  *         用同一个IP多播地址接收多播数据包的所有主机构成了一个主机组，也称为多播组。一个多播组的成员是随时变动的，一台主机可以随时加入或离开多播组，多播组成员的数目和所在的地理位置也不受限制，一台主机也可以属于几个多播组。
- * 此外，不属于某一个多播组的主机也可以向该多播组发送数据包。  
- * 
+ * 此外，不属于某一个多播组的主机也可以向该多播组发送数据包。
+ *
  * 组播地址
  * 组播组可以是永久的也可以是临时的。组播组地址中，有一部分由官方分配的，称为永久组播组。
  * 永久组播组保持不变的是它的ip地址，组中的成员构成可以发生变化。永久组播组中成员的数量都可以是任意的，甚至可以为零。那些没有保留下来供永久组播组使用的ip组播地址，可以被临时组播组利用。
@@ -331,7 +338,7 @@ int udp_get_boardcast(ncb_t *ncb, int *enabled) {
  *      224.0.1.0～224.0.1.255是公用组播地址，可以用于Internet
  *      224.0.2.0～238.255.255.255为用户可用的组播地址（临时组地址），全网范围内有效
  *      239.0.0.0～239.255.255.255为本地管理组播地址，仅在特定的本地范围内有效
- * 
+ *
  *  组播是一对多的传输方式，其中有个组播组的 概念，发送端将数据向一个组内发送，网络中的路由器通过底层的IGMP协议自动将数据发送到所有监听这个组的终端。
  *  至于广播则和组播有一些相似，区别是路由器向子网内的每一个终端都投递一份数据包，不论这些终端是否乐于接收该数据包。UDP广播只能在内网（同一网段）有效，而组播可以较好实现跨网段群发数据。
  *   UDP组播是采用的无连接,数据报的连接方式，所以是不可靠的。也就是数据能不能到达接受端和数据到达的顺序都是不能保证的。但是由于UDP不用保证数据 的可靠性，所有数据的传送效率是很快的。
@@ -362,18 +369,18 @@ int udp_joingrp(HUDPLINK lnk, const char *g_ipstr, uint16_t g_port) {
         if (retval < 0) {
             break;
         }
-        
+
         /* insert into multicast group */
         if (!ncb->u.udp.mreq){
             ncb->u.udp.mreq = (struct ip_mreq *)malloc(sizeof(struct ip_mreq));
         }
-        ncb->u.udp.mreq->imr_multiaddr.s_addr = inet_addr(g_ipstr); 
-        ncb->u.udp.mreq->imr_interface.s_addr = ncb->local_addr.sin_addr.s_addr; 
+        ncb->u.udp.mreq->imr_multiaddr.s_addr = inet_addr(g_ipstr);
+        ncb->u.udp.mreq->imr_interface.s_addr = ncb->local_addr.sin_addr.s_addr;
         retval = setsockopt(ncb->sockfd, IPPROTO_IP, IP_ADD_MEMBERSHIP, (const void *)ncb->u.udp.mreq, sizeof(struct ip_mreq));
         if (retval < 0){
             break;
         }
-        
+
     } while (0);
 
     objdefr(lnk);
@@ -383,7 +390,7 @@ int udp_joingrp(HUDPLINK lnk, const char *g_ipstr, uint16_t g_port) {
 int udp_dropgrp(HUDPLINK lnk){
     ncb_t *ncb;
     int retval;
-    
+
     retval = udprefr(lnk, &ncb);
     if (retval < 0) {
         return retval;
@@ -391,23 +398,23 @@ int udp_dropgrp(HUDPLINK lnk){
 
     do{
         retval = -1;
-        
+
         if (!(ncb->u.udp.flag & UDP_FLAG_MULTICAST) || !ncb->u.udp.mreq) {
             break;
         }
-        
+
         /* reduction permit for loopback */
         int loop = 0;
         retval = setsockopt(ncb->sockfd, IPPROTO_IP, IP_MULTICAST_LOOP, (const void *)&loop, sizeof (loop));
         if (retval < 0) {
             break;
         }
-        
+
         /* leave multicast group */
         retval = setsockopt(ncb->sockfd, IPPROTO_IP, IP_DROP_MEMBERSHIP, (const void *)ncb->u.udp.mreq, sizeof(struct ip_mreq));
-        
+
     }while(0);
-    
+
     objdefr(lnk);
     return retval;
 }
