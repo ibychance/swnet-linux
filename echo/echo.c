9f139793ea11d398c839dc17254a97894ebf7839
zhuoyunzhi <250603123@qq.com>
2019-11-26 15:12:26 +0800

    完善echo的测试用例

--- echo/echo.c
+++ echo/echo.c
@@ -3,30 +3,42 @@
 #include "nis.h"
 #include "posix_wait.h"
 #include "posix_ifos.h"
+#include "posix_naos.h"
 #include "logger.h"
 
 #include <stdio.h>
 
-/*#define echo_logger_info(context) log__save("nshost.echo", kLogLevel_Info, kLogTarget_Stdout | kLogTarget_Filesystem, #context)*/
+#define ECHO(fmt, arg...) log__save("nshost.echo", kLogLevel_Info, kLogTarget_Stdout | kLogTarget_Filesystem, fmt, ##arg)
 
-void nshost_tcp_callback(const struct nis_event *event, const void *data)
+int display(HTCPLINK link, const unsigned char *data, int size)
 {
-	int n;
-	int size;
 	char output[1024];
+	uint32_t ip;
+	uint16_t port;
+	char ipstr[INET_ADDRSTRLEN];
+	int offset;
+
+	tcp_getaddr(link, LINK_ADDR_REMOTE, &ip, &port);
+	posix__ipv4tos(ip, ipstr, sizeof(ipstr));
+	offset = sprintf(output, "[income %s:%u] ", ipstr, port);
+
+	if (size < (sizeof(output) - offset) && size > 0) {
+		memcpy(&output[offset], data, size);
+		return posix__file_write(1, output, size + offset);
+	}
+
+	return -1;
+}
+
+void tcp_server_callback(const struct nis_event *event, const void *data)
+{
 	struct nis_tcp_data *tcpdata = (struct nis_tcp_data *)data;
 	HTCPLINK link;
 
 	link = event->Ln.Tcp.Link;
 	switch(event->Event) {
 		case EVT_RECEIVEDATA:
-			size = tcpdata->e.Packet.Size;
-			if (size < 1020 && size > 0) {
-				memcpy(output, ">>> ", 4);
-				memcpy(output + 4, tcpdata->e.Packet.Data, tcpdata->e.Packet.Size);
-				output[tcpdata->e.Packet.Size + 4] = 0;
-				n = 4 + tcpdata->e.Packet.Size + 1;
-				posix__file_write(1,output, n);
+			if (display(link, tcpdata->e.Packet.Data, tcpdata->e.Packet.Size) > 0 ) {
 				if (tcp_write(link, tcpdata->e.Packet.Data, tcpdata->e.Packet.Size, NULL) <= 0) {
 					tcp_destroy(link);
 				}
@@ -38,8 +50,26 @@ void nshost_tcp_callback(const struct nis_event *event, const void *data)
 			break;
 		case EVT_CLOSED:
 			break;
+		default:
+			break;
+	}
+}
+
+void tcp_client_callback(const struct nis_event *event, const void *data)
+{
+	struct nis_tcp_data *tcpdata = (struct nis_tcp_data *)data;
+
+	switch(event->Event) {
+		case EVT_RECEIVEDATA:
+			display(event->Ln.Tcp.Link, tcpdata->e.Packet.Data, tcpdata->e.Packet.Size);
+			posix__file_write(1, "input:$ ", 8);
+			break;
+		case EVT_TCP_ACCEPTED:
+			break;
+		case EVT_CLOSED:
+			break;
 		case EVT_TCP_CONNECTED:
-			log__save("nshost.echo", kLogLevel_Info, kLogTarget_Stdout | kLogTarget_Filesystem,"client link "UINT64_STRFMT" connected.", link);
+			posix__file_write(1, "input:$ ", 8);
 			break;
 		default:
 			break;
@@ -49,7 +79,7 @@ void nshost_tcp_callback(const struct nis_event *event, const void *data)
 void nshost_ecr(const char *host_event, const char *reserved, int rescb)
 {
 	if (host_event) {
-		log__save("nshost.echo", kLogLevel_Info, kLogTarget_Stdout | kLogTarget_Filesystem, "%s", host_event);
+		ECHO("%s", host_event);
 	}
 }
 
@@ -57,40 +87,41 @@ int echo_server_startup(const char *host, uint16_t port)
 {
 	HTCPLINK server;
 
-	log__save("nshost.echo", kLogLevel_Info, kLogTarget_Stdout | kLogTarget_Filesystem, "server host %s:%u", host, port);
-
-	server = tcp_create(&nshost_tcp_callback, host, port);
+	server = tcp_create(&tcp_server_callback, host, port);
 	if (INVALID_HTCPLINK == server) {
 		return 1;
 	}
 
 	tcp_listen(server, 100);
-
 	posix__hang();
 	return 0;
 }
 
 int echo_client_startup(const char *host, uint16_t port)
 {
-	int i;
-	HTCPLINK client[2];
+	HTCPLINK client;
+	char text[1024], *p;
+	size_t n;
 
-	log__save("nshost.echo", kLogLevel_Info, kLogTarget_Stdout | kLogTarget_Filesystem, "clinet target host %s:%u", host, port);
-
-	for (i = 0; i < 2; i++) {
-		client[i] = tcp_create(&nshost_tcp_callback, NULL, 0);
-		if (INVALID_HTCPLINK == client[i]) {
-			return 1;
+	do {
+		client = tcp_create(&tcp_client_callback, NULL, 0);
+		if (INVALID_HTCPLINK == client) {
+			break;
 		}
-		log__save("nshost.echo", kLogLevel_Info, kLogTarget_Stdout | kLogTarget_Filesystem,"client link "UINT64_STRFMT" created.", client[i]);
-	}
 
-	for (i = 0; i < 2; i++) {
-		tcp_connect2(client[i], host, port);
-	}
+		if (tcp_connect2(client, host, port) < 0) {
+			break;
+		}
 
-	posix__hang();
-	return 0;
+		while ( NULL != (p = fgets(text, sizeof(text), stdin)) ) {
+			n = strlen(text);
+			if (tcp_write(client, text, n, NULL) < 0) {
+				break;
+			}
+		}
+	} while( 0 );
+
+	return 1;
 }
 
 int main(int argc, char **argv)
@@ -106,8 +137,11 @@ int main(int argc, char **argv)
 	}
 
 	log__init();
-	nis_checr(&nshost_ecr);
 	tcp_init();
+
+#if _SET_ECR
+	nis_checr(&nshost_ecr);
+#endif
 
 	if (type == SESS_TYPE_SERVER) {
 		return echo_server_startup(gethost(), getport());
